9228872099088e19826c0685e36020be8a59617c
Shlok <shlok.24bcs10322@sst.scaler.com>
2026-03-23 15:46:54 +0530

    SnakeAndLadder

---
+++ SnakeAndLadder/SnakeAndLadderDemo.java
@@ -0,0 +1,30 @@
+package SnakeAndLadder;
+
+import java.util.Arrays;
+import java.util.List;
+
+import SnakeAndLadder.models.BoardEntity;
+import SnakeAndLadder.models.Dice;
+import SnakeAndLadder.models.Ladder;
+import SnakeAndLadder.models.Snake;
+
+public class SnakeAndLadderDemo {
+    public static void main(String[] args) {
+        List<BoardEntity> boardEntities = List.of(
+                new Snake(17, 7), new Snake(54, 34),
+                new Snake(62, 19), new Snake(98, 79),
+                new Ladder(3, 38), new Ladder(24, 33),
+                new Ladder(42, 93), new Ladder(72, 84)
+        );
+
+        List<String> players = Arrays.asList("Alice", "Bob", "Charlie");
+
+        Game game = new Game.Builder()
+                .setBoard(100, boardEntities)
+                .setPlayers(players)
+                .setDice(new Dice(1, 6))
+                .build();
+
+        game.play();
+    }
+}
